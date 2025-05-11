a68fe95a1a9641dc216095b0e33913cff0ae9d63
Ukenn <duola2002@gmail.com>
2025-05-11 19:13:00 +0900

    Initial Commit

---
+++ anitabi/anitabiApp.swift
@@ -0,0 +1,17 @@
+//
+//  anitabiApp.swift
+//  anitabi
+//
+//  Created by 维安雨轩 on 2025/05/11.
+//
+
+import SwiftUI
+
+@main
+struct anitabiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
